d8f82cc59a094b5ba049216314d27fafd08cab17
Benkolov <111705203+Benkolov@users.noreply.github.com>
2022-09-09 19:54:38 +0300

    Create day_of_week.py

---
+++ python_basics_2022/conditional_statements_advanced_lab/day_of_week.py
@@ -0,0 +1,20 @@
+dey = int(input())
+if dey == 1:
+    print("Monday")
+elif dey == 2:
+    print("Tuesday")
+elif dey == 3:
+    print("Wednesday")
+elif dey == 4:
+    print("Thursday")
+elif dey == 5:
+    print("Friday")
+elif dey == 6:
+    print("Saturday")
+elif dey == 7:
+    print("Sunday")
+else:
+    print("Error")
+
+
+
